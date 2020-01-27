16046a01c0d2a242d8ba756ebecaa86cbe4d17fa
12576 <1257672241@qq.com>
2020-01-27 15:45:09 +0800

    test

---
+++ src/HelloWorld.java
@@ -0,0 +1,11 @@
+/**
+ * @program: git
+ * @author: Semineces
+ * @create: 2020-01-27 15:43
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("HelloWorld");
+    }
+}
